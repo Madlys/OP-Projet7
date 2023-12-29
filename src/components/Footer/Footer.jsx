3b0803de3a7545336d44a6f1337402d9e9ee118f
Clarisse <78051319+Madlys@users.noreply.github.com>
2023-12-29 18:41:06 +0100

    footer ok

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,10 @@
 import './Footer.scss'
+import logo from '../../assets/logoKasaFooter.png'
 
 function Footer() {
     return <div id="footer">
-
+        <img id='logoFooter' src={logo} alt="logo Kaza" />
+        <p id='footerText'>© 2020 Kasa. All rights reserved</p>
     </div>
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
 import './Header.scss'
-import logo from '../../assets/logoKasa.png'
+import logo from '../../assets/logoKasaHeader.png'
 
 function Header() {
     return <div id="header">
-        <img className='logo' src={logo} alt='logo Kasa'></img>
+        <img id='logoHeader' src={logo} alt='logo Kasa'></img>
         <div id="header-link">
             <a href='' id='accueilLink'>Accueil</a>
             <a href='' id='aproposLink'>A propos</a>
