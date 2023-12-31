068ead265c2eb925478937b7484642aa49579c0b
Clarisse <78051319+Madlys@users.noreply.github.com>
2023-12-31 19:21:40 +0100

    card ok

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,15 @@
 import './Card.scss'
+import logements from '../../logements.json'
 
 function Card() {
-    return <div id="card">
-
+    return <div id="cards">
+        {logements.map((logement) =>
+            <a href='' className='card'>
+                <img className='cardImg' src={logement.cover} alt="photo du logement"></img>
+                <div className='cardGradient'></div>
+                <div id={logement.id} className='cardTitle'>{logement.title}</div>
+            </a>
+        )}
     </div>
 }
 
