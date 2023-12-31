967e3ee3073d2014bc4676f0ee7b2b8e94dcab36
Clarisse <78051319+Madlys@users.noreply.github.com>
2023-12-31 20:30:19 +0100

    router init

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import './Header.scss'
 import logo from '../../assets/logoKasaHeader.png'
+import { Link } from 'react-router-dom'
 
 function Header() {
     return <div id="header">
         <img id='logoHeader' src={logo} alt='logo Kasa'></img>
         <div id="header-link">
-            <a href='' id='accueilLink'>Accueil</a>
-            <a href='' id='aproposLink'>A propos</a>
+            <Link to='/' id='accueilLink'>Accueil</Link>
+            <Link to='/about' id='aproposLink'>A propos</Link>
         </div>
     </div>
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home';
+import About from './pages/About/About';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+     <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
