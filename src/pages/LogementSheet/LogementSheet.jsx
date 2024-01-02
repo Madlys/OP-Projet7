141772739b7166ef80c9a55a002111e1e550efa4
Clarisse <78051319+Madlys@users.noreply.github.com>
2024-01-02 20:26:05 +0100

    logementSheet title & Location ok

--- src/pages/LogementSheet/LogementSheet.jsx
+++ src/pages/LogementSheet/LogementSheet.jsx
@@ -1,15 +1,20 @@
+import "./LogementSheet.scss"
 import Header from "../../components/Header/Header";
 import Carrousel from "../../components/Carrousel/Carrousel";
 import logements from "../../logements.json";
 import { useParams } from "react-router-dom";
 
-function Logement(prop) {
+function Logement() {
     const {id} = useParams();
     const logement = logements.find((logement) => logement.id == id);
     return (
         <div>
             <Header></Header>
             <Carrousel logement={logement}></Carrousel>
+            <div id="logementText">
+                <h1 id="logementTitle">{logement.title}</h1>
+                <p id="logementLocation">{logement.location}</p>
+            </div>
         </div>
     )
 }
