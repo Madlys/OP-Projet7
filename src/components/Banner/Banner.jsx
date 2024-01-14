3550afd59661a34d2726edd6fde3d23418a1f975
Clarisse <78051319+Madlys@users.noreply.github.com>
2024-01-14 12:19:54 +0100

    banner component responsivity adjustments + collapse component responsivity OK+ About page responsivity OK

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,8 +3,8 @@ import './Banner.scss'
 function Banner({id, banner, title}) {
     return <header id={id}>
         <img className='bannerImg' src={banner} alt="banner image"></img>
+        <div className='bannerBackground'></div>
         {/* if home -> text */}
-        {title ? <div className='bannerTextBackground'></div> : null}
         {title ? <div className='bannerText'>{title}</div> : null}
     </header>
 }
